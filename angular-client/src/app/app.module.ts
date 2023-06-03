9e6458d18d5ecd89881acddba8ad9a44d9897edc
marom83 <marom83@gmail.com>
2023-06-03 11:36:00 +0200

    fix

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { ApiModule, UserService } from 'top-movies-client-api';
+import { ApiModule } from 'top-movies-client-api';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
